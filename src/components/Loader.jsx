d8edff3ca791be5091d079a8362dada8f4a393bf
Tanya Yenakieva <yastigma@gmail.com>
2025-05-09 12:36:07 +0300

    fix: loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,17 +1,8 @@
-import { ThreeCircles } from "react-loader-spinner";
-
+import { ClipLoader } from "react-spinners";
 export default function Loader() {
   return (
     <div className="fixed inset-0 flex justify-center items-center  bg-opacity-50 z-50">
-      <ThreeCircles
-        visible={true}
-        height="100"
-        width="100"
-        color="#3b82f6"
-        ariaLabel="three-circles-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
+      <ClipLoader color="#3B82F6" size={50} />
     </div>
   );
 }
